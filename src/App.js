5b82ca75b322c047f046112e065250e7b3b01837
dieduro <diegomduro@gmail.com>
2018-05-13 18:23:17 -0300

    Added Redux, PropTypes, first reducer and container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Gato! Alto juego con react, redux and all that shit</h1>
+        <h1>{this.props.message}</h1>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired
+};
 
 export default App;
