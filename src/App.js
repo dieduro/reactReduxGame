3ce1cef208e81a92496d2181263cbd7ffa1051ed
dieduro <diegomduro@gmail.com>
2018-05-13 17:47:37 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Gato! Alto juego con react, redux and all that shit</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
